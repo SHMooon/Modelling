fca3239d5753f0df2c3c6041b255bb449bb7e476
SHMooon <SHMooon@users.noreply.github.com>
2023-05-24 13:04:49 +0200

    add

--- things.R
+++ things.R
@@ -1,1 +1,13 @@
-#groundnut: emergence date 90 because under 90 and over 90 is not suitable. 
+#in sevilla
+  #groundnut: emergence date 90 because under 90 and over 90 is not suitable. 
+  #maize. emergence date will be 57. under or over 57 is not suitable. -> less yield. 
+
+
+
+#we will chose sevilla because there are more yield from more radiation? 
+
+#explain the situation maize in wageningen: from 30 to 70 same yield 1125... 
+#however situation maize in wageningen: 29 -> 1126
+#what is the meaing of the range of the emergence date like in wageningen is huge range... 
+
+
