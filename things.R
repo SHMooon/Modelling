bbc91e718e4f622f8d2044d2b1400e3d6b18d062
Noviria Syifaun Nafsi <noviriasyifaunnafsi@gmail.com>
2023-06-09 12:13:13 +0200

    commit

--- things.R
+++ things.R
@@ -12,11 +12,18 @@
 #explain the situation maize in wageningen: from 30 to 70 same yield 1125... 
 #however situation maize in wageningen: 29 -> 1126
 #what is the meaing of the range of the emergence date like in wageningen is huge range... 
-#in wageningen the ground nut will be yielded at feburary next year.
+#in wageningen the ground nut will be yielded at february next year.
   #so it is not optimal for next crop on the same field. 
   #and to grown up the goundnut in wageningen takes longer. 
 
+# LER --> Land Equivalent Ratio
+# LER --> (Yield_interCR1 / Yield_interCR2) + (Yield_monoCR1 / Yield_monoCR2)
 
-a <- (1402/1815)+(568/897)
-a
+LER <- (1402/1815)+(568/897)
+LER
+
+# if the value is more than 1, it means that it's good (?)
+
+
+
 
