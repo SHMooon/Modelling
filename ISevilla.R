ff291d96d58c24e1c4b87786e4101aa65b0e2ed1
Noviria Syifaun Nafsi <noviriasyifaunnafsi@gmail.com>
2023-07-03 16:12:01 +0200

    done

--- ISevilla.R
+++ ISevilla.R
@@ -7,4 +7,144 @@ Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_70_W0_5 <-
          sep=";")
 
 
-MaxIM57G70_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_70_W0_5$TotalYield_per_sqm))
+MaxIM57G70_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_70_W0_5$Yield1_per_sqm))
+MaxIG57G70_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_70_W0_5$Yield2_per_sqm))
+
+
+# 2
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_80_W0_5 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-80_W0.5.csv",
+         sep=";")
+
+MaxIM57G80_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_80_W0_5$Yield1_per_sqm))
+MaxIG57G80_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_80_W0_5$Yield2_per_sqm))
+
+
+# 3
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_3 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.3.csv",
+           sep=";")
+
+MaxIM57G90_0.3 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_3$Yield1_per_sqm))
+MaxIG57G90_0.3 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_3$Yield2_per_sqm))
+
+
+# 4
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_4 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.4.csv",
+           sep=";")
+
+MaxIM57G90_0.4 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_4$Yield1_per_sqm))
+MaxIG57G90_0.4 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_4$Yield2_per_sqm))
+
+
+# 5
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_5 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.5.csv",
+           sep=";")
+
+MaxIM57G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_5$Yield1_per_sqm))
+MaxIG57G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_5$Yield2_per_sqm))
+
+
+# 6
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_6 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.6.csv",
+           sep=";")
+
+MaxIM57G90_0.6 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_6$Yield1_per_sqm))
+MaxIG57G90_0.6 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_6$Yield2_per_sqm))
+
+
+# 7
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_6 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.6.csv",
+           sep=";")
+
+MaxIM57G90_0.6 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_6$Yield1_per_sqm))
+MaxIG57G90_0.6 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_6$Yield2_per_sqm))
+
+
+# 8
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_9 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.9.csv",
+           sep=";")
+
+MaxIM57G90_0.9 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_9$Yield1_per_sqm))
+MaxIG57G90_0.9 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_9$Yield2_per_sqm))
+
+
+# 9
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_35 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.35.csv",
+           sep=";")
+
+MaxIM57G90_0.35 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_35$Yield1_per_sqm))
+MaxIG57G90_0.35 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_35$Yield2_per_sqm))
+
+
+
+# 10
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_36 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.36.csv",
+           sep=";")
+
+MaxIM57G90_0.36 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_36$Yield1_per_sqm))
+MaxIG57G90_0.36 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_36$Yield2_per_sqm))
+
+
+
+# 11
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_45 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.45.csv",
+           sep=";")
+
+MaxIM57G90_0.45 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_45$Yield1_per_sqm))
+MaxIG57G90_0.45 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_45$Yield2_per_sqm))
+
+
+# 12
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_45 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.45.csv",
+           sep=";")
+
+MaxIM57G90_0.45 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_45$Yield1_per_sqm))
+MaxIG57G90_0.45 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_45$Yield2_per_sqm))
+
+
+# 13
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_55 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57-90_W0.55.csv",
+           sep=";")
+
+MaxIM57G90_0.55 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_55$Yield1_per_sqm))
+MaxIG57G90_0.55 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start57_90_W0_55$Yield2_per_sqm))
+
+
+# 14
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start60_90_W0_5 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start60-90_W0.5.csv",
+           sep=";")
+
+MaxIM60G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start60_90_W0_5$Yield1_per_sqm))
+MaxIG60G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start60_90_W0_5$Yield2_per_sqm))
+
+
+# 15
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start67_90_W0_5 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start67-90_W0.5.csv",
+           sep=";")
+
+MaxIM67G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start67_90_W0_5$Yield1_per_sqm))
+MaxIG67G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start67_90_W0_5$Yield2_per_sqm))
+
+
+
+# 16
+Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start99_90_W0_5 <- 
+  read.csv("2Intercrop-Sevilla/Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start99-90_W0.5.csv",
+           sep=";")
+
+MaxIM99G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start99_90_W0_5$Yield1_per_sqm))
+MaxIG99G90_0.5 <- print(max(Lintul5_Intercropping_sevilla_maize_groundnut_daily_Start99_90_W0_5$Yield2_per_sqm))
+
